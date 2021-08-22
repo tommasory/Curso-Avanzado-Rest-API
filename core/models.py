198b44e0e24b26051a9d84a0850926d09076ea43
Tommasory <tommasory@gmail.com>
2021-08-22 16:16:25 -0500

    Agrega modelo personalizado de usuario.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ Crea y guarda un nuevo usuario """
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+        
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ Modelo personalidad de usuario que soporta hacer login con Email en lugar de usuario """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+

ec26bdc19d96a71fbfbcf68df4a07e99b60cedb3
Tommasory <tommasory@gmail.com>
2021-08-22 14:40:14 -0500

    Instalando App Core.

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'core',
+]
+
+THIRD_APPS = [
+    'rest_framework.authtoken',
+    'rest_framework',
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

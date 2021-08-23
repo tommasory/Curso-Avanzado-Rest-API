23c0b21f28e21954d14d47284adaea540039c93b
Tommasory <tommasory@gmail.com>
2021-08-23 18:02:00 -0500

    Agrega Test y Solución para Agregar Usuario desde Admin.

--- core/admin.py
+++ core/admin.py
@@ -17,4 +17,11 @@ class UserAdmin(BaseUserAdmin):
         (_('Imporant Dates'),{'fields':('last_login', )})
     )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 admin.site.register(models.User, UserAdmin)

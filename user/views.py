6598a2661df8a461878d7ac0bc1ed1cc683de757
Tommasory <tommasory@gmail.com>
2021-08-23 21:35:18 -0500

    Agrega solución para Test API para crear usuarios.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    """ Crear nuevo usuario en el sistema """
+    serializer_class = UserSerializer

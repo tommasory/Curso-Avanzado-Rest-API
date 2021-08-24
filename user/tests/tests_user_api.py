5efd028576c18972703be393ac8f366e8d010c84
Tommasory <tommasory@gmail.com>
2021-08-24 08:42:23 -0500

    Agrega Tests para crear TOKEN del API.

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -62,3 +63,52 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """ Probar que el token sea creado por el usuario """
+        payload = {
+            'email':'test@gmail.com',
+            'password':'DataScience2021',
+            'name':'Test name'
+        }
+
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """ Probar que el token no es creado con credenciales invalidas """
+
+        create_user(email='test@gmail.com',password='DataScience2021')
+        payload = {
+            'email':'test@gmail.com',
+            'password':'unicauca'
+        }
+        
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """ Prueba que no se crea token si no existe el usuario """
+        payload = {
+            'email':'test@gmail.com',
+            'password':'DataScience2021',
+            'name':'Test name'
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """ Probar que el email y contraseña sean requeridos """
+        res = self.client.post(TOKEN_URL, {'email':'test@','password':''})
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    

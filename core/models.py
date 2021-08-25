b69c1153e8498ffcee7f7b0732802f0fc60bc507
Tommasory <tommasory@gmail.com>
2021-08-24 22:00:45 -0500

    Agrega Test y solucón para crear Modelo de Ingredientes.

--- core/models.py
+++ core/models.py
@@ -53,6 +53,17 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Ingredient(models.Model):
+    """ Ingrediente para usarse en la receta """
+    name  = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
     
 
 
--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -56,3 +56,12 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """ Probar representación en cadena de texto del los ingredientes """
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name = 'Banana'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)

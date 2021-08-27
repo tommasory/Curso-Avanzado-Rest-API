d4399359c7cb38c185a2b501c618e13ba843a463
Tommasory <tommasory@gmail.com>
2021-08-27 11:25:49 -0500

    Agrega campo de imagen para que se retorne el link de las recetas.

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = (
-            'id', 'title', 'ingredients', 'tags', 'time_minutes', 'price',
+            'id', 'title','image', 'ingredients', 'tags', 'time_minutes', 'price',
             'link',
         )
         read_only_fields = ('id', )
